2c3f404f89b42e95077ad5042d8fb07139fceab9
Chen <qiaochen@outlook.com>
2022-12-29 20:50:24 +0800

    update sparkx r script

---
+++ notebooks/sparkX.r
@@ -0,0 +1,18 @@
+library(SPARK)
+loc_path <- '../../output/locations/melanoma.csv'
+
+cal_sparkX <- function(in_path, out_path) {
+    df <- t(subset(read.csv(in_path), select = -c(X)))
+    loc = read.csv(loc_path)
+    rownames(loc) <- colnames(df)
+    sp_res <- sparkx(df, loc, numCores=10, option='mixture')
+    write.csv(sp_res$res_mtest, out_path)
+}
+
+in_prefix <- "../../output/melanomaext_singlecell_"
+methods <- c("transImpute", "transImpSpa", "transImpCls", "transImpClsSpa", "spaGE", "stPlus", "Tangram")
+for (mt in methods){
+    in_path <- paste0(in_prefix, mt, '.csv')
+    out_path <- paste0("../../output/sparkx_melanoma_", mt, ".csv")
+    cal_sparkX(in_path, out_path)
+}
