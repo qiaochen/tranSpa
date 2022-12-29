3a7c6105f97b7fcecede3a98ff5172366baba911
Chen <qiaochen@outlook.com>
2022-12-29 20:50:40 +0800

    update sparkx script

--- notebooks/sparkX.r
+++ notebooks/sparkX.r
@@ -12,6 +12,7 @@ cal_sparkX <- function(in_path, out_path) {
 in_prefix <- "../../output/melanomaext_singlecell_"
 methods <- c("transImpute", "transImpSpa", "transImpCls", "transImpClsSpa", "spaGE", "stPlus", "Tangram")
 for (mt in methods){
+    print(mt)
     in_path <- paste0(in_prefix, mt, '.csv')
     out_path <- paste0("../../output/sparkx_melanoma_", mt, ".csv")
     cal_sparkX(in_path, out_path)
