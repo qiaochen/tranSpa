c9950cdcee2308a4cb84d996a2614c09588adda1
Chen <qiaochen.reg@outlook.com>
2023-07-21 13:00:21 +0800

    Update conf.py
    Former-commit-id: a2c5fcacd98b90fa8c2309d978545ced73f580c5 [formerly a2c5fcacd98b90fa8c2309d978545ced73f580c5 [formerly 544a246c51697ed444ff148a0710a39448cfc250]]
    Former-commit-id: 66addbd870445dabecaa664752269fad28d54aad
    Former-commit-id: ac799164d856f12e1ce1b3fdef784b6b743c91c6
    Former-commit-id: 1d753c390edfba1bce08bd692e4235fbe38bbdcf

--- docs/conf.py
+++ docs/conf.py
@@ -10,26 +10,46 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+import sys
+import os
+from datetime import datetime
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+from pathlib import Path
+from sphinx.application import Sphinx
+from sphinx.ext import autosummary
+from urllib.request import urlretrieve
 
+HERE = Path(__file__).parent
+sys.path.insert(0, f"{HERE.parent.parent}")
+sys.path.insert(0, os.path.abspath("_ext"))
 
 # -- Project information -----------------------------------------------------
 
 project = 'TransImp'
-copyright = '2023, Chen Qiao, Yuanhua Huang'
 author = 'Chen Qiao, Yuanhua Huang'
-
+copyright = f"{datetime.now():%Y}, {author}"
 # The full version, including alpha/beta/rc tags
 release = '0.1.0'
 
+notebooks_url = "https://github.com/qiaochen/tranSpa/raw/main/demo/"
+notebooks = [
+    "seqfish.ipynb",
+]
+for nb in notebooks:
+    try:
+        urlretrieve(notebooks_url + nb, nb)
+    except:
+        raise ValueError(f'{nb} cannot be retrieved.')
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+needs_sphinx = "1.7"
+
 extensions = [
   "sphinx.ext.autodoc",
   "sphinx.ext.doctest",
@@ -43,24 +63,77 @@ extensions = [
   "nbsphinx",
 ]
 
+
+
 autosummary_generate = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
+source_suffix = ['.rst']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
+master_doc = 'index'
+enable_pdf_build = False
+enable_epub_build = False
+
+def setup(app):
+    """TODO."""
+    app.add_css_file("custom.css")
+
+def process_generate_options(app: Sphinx):
+    """TODO."""
+    genfiles = app.config.autosummary_generate
+
+    if genfiles and not hasattr(genfiles, "__len__"):
+        env = app.builder.env
+        genfiles = [
+            env.doc2path(x, base=None)
+            for x in env.found_docs
+            if Path(env.doc2path(x)).is_file()
+        ]
+    if not genfiles:
+        return
+
+    from sphinx.ext.autosummary.generate import generate_autosummary_docs
+
+    ext = app.config.source_suffix
+    genfiles = [
+        genfile + (not genfile.endswith(tuple(ext)) and ext[0] or "")
+        for genfile in genfiles
+    ]
+
+    suffix = autosummary.get_rst_suffix(app)
+    if suffix is None:
+        return
+
+    generate_autosummary_docs(
+        genfiles,
+        builder=app.builder,
+        warn=logger.warning,
+        info=logger.info,
+        suffix=suffix,
+        base_path=app.srcdir,
+        imported_members=True,
+        app=app,
+    )
+
+
+autosummary.process_generate_options = process_generate_options
+
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+pygments_style = 'sphinx'
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
-
+github_repo = 'tranSpa'
+github_nb_repo = 'tranSpa'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_theme_options = dict(navigation_depth=1, titles_only=True)
+htmlhelp_basename = 'tranSpa'
