5da7fc115eff27252c81e731b4847bf6e2f49c02
Qiao <cqiao@sgnt-prd-brln03.cm.cluster>
2023-07-21 15:59:04 +0800

    update css

--- docs/conf.py
+++ docs/conf.py
@@ -139,5 +139,11 @@ github_nb_repo = 'tranSpa'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
 html_theme_options = dict(navigation_depth=1, titles_only=True)
 htmlhelp_basename = 'tranSpa'
