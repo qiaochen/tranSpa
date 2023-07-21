f7fea25d6e822515f7c1edf7615e6788443aa970
Chen <qiaochen.reg@outlook.com>
2023-07-21 11:32:53 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,19 @@ release = '0.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+  "sphinx.ext.autodoc",
+  "sphinx.ext.doctest",
+  "sphinx.ext.coverage",
+  "sphinx.ext.mathjax",
+  "sphinx.ext.autosummary",
+  "sphinx.ext.napoleon",
+  "sphinx.ext.intersphinx",
+  "sphinx.ext.githubpages",
+  "sphinx_autodoc_typehints",
+  "nbsphinx",
 ]
 
+autosummary_generate = True
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
